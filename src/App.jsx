f2bcb4b5b63ff1de5866c09ee516a043091a35a2
krgarav <krgarav@gmail.com>
2024-04-15 17:50:00 +0530

    Added all compulsory field

--- src/App.jsx
+++ src/App.jsx
@@ -3,33 +3,45 @@ import Input from "./UI/Input";
 import Fab from "@mui/material/Fab";
 import CompareArrowsIcon from "@mui/icons-material/CompareArrows";
 import OptimisedList from "./UI/OptimisedList";
+import Button from "@mui/material/Button";
+import "./App.css";
 function App() {
   return (
     <>
-      <main className="flex flex-col m-3 p-4 gap-5 border-dashed border-4  ">
-        <div className="">
-          <h1 className="text-center m-5">Match and compare data</h1>
-          <div className="flex flex-row justify-between  gap-10 mb-6">
-            <Input label="Select Paper 1" />
-            <Input label="Select Paper 2" />
+      {/* <section className="bg-slate-300 w-full h-[100vh] m-0 p-0"> */}
+        <main className="flex flex-col m-3 p-4 gap-5 bg-white rounded-md">
+          <div className="border-dashed p-3 border-4 rounded-md">
+
+          <div className="">
+            <h1 className="text-center m-5">Match and compare data</h1>
+            <div className="flex flex-row justify-between  gap-10 mb-6">
+              <Input label="Select Paper 1" />
+              <Input label="Select Paper 2" />
+            </div>
+            <Customselect label="Select Primary Key" />
+          </div>
+          <div className="flex justify-between">
+            <div className="border pl-2 pb-2  bg-slate-100 rounded">
+              <div className="flex flex-row pt-2 pb-2 justify-between self-center ">
+                <p className="text-sm font-semibold align-bottom self-center ">
+                  Select Key For Skipping Comparison
+                </p>
+                <Button>Clear All</Button>
+              </div>
+              <OptimisedList />
+            </div>
+
+            <div className="flex self-end">
+              <Fab variant="extended" color="primary">
+                <CompareArrowsIcon sx={{ mr: 1 }} />
+                Compare And Match
+              </Fab>
+            </div>
+          </div>
           </div>
 
-          <Customselect label="Select Primary Key" />
-        </div>
-        <div className="flex justify-between">
-          <div className="border pl-5 pt-5">
-            <h2>Select key for skipping comparison </h2>
-            <OptimisedList />
-          </div>
-
-          <div className="flex self-end">
-            <Fab variant="extended" color="primary">
-              <CompareArrowsIcon sx={{ mr: 1 }} />
-              Compare And Match
-            </Fab>
-          </div>
-        </div>
-      </main>
+        </main>
+      {/* </section> */}
     </>
   );
 }
--- src/UI/Customselect.jsx
+++ src/UI/Customselect.jsx
@@ -4,9 +4,18 @@ import MenuItem from "@mui/material/MenuItem";
 import FormHelperText from "@mui/material/FormHelperText";
 import FormControl from "@mui/material/FormControl";
 import Select from "@mui/material/Select";
+
 const Customselect = (props) => {
+  const arr = new Array(50).fill("10").map((item, index) => {
+    return (
+      <MenuItem key={index} value={10}>
+        Ten
+      </MenuItem>
+    );
+  });
+  console.log(arr);
   return (
-    <FormControl required sx={{  width: "100%" }}>
+    <FormControl required sx={{ width: "100%" }}>
       <InputLabel id="demo-simple-select-required-label">
         {props.label}
       </InputLabel>
@@ -15,14 +24,19 @@ const Customselect = (props) => {
         id="demo-simple-select-required"
         // value={age}
         label={`${props.label} *`}
+        MenuProps={{
+          anchorOrigin: { vertical: "bottom", horizontal: "left" },
+          transformOrigin: { vertical: "top", horizontal: "left" },
+          getContentAnchorEl: null,
+          PaperProps: {
+            style: {
+              maxHeight: "200px", // Adjust the maximum height of the dropdown menu
+            },
+          },
+        }}
         // onChange={handleChange}
       >
-        {/* <MenuItem value="">
-          <em>None</em>
-        </MenuItem> */}
-        <MenuItem value={10}>Ten</MenuItem>
-        <MenuItem value={20}>Twenty</MenuItem>
-        <MenuItem value={30}>Thirty</MenuItem>
+        {arr}
       </Select>
       <FormHelperText>Required</FormHelperText>
     </FormControl>
